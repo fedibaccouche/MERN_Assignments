09dd1aa6376549516384bb6cd84fb840f68c6a6a
fedibaccouche <fedi.baccouche1@esprit.tn>
2024-01-15 03:04:44 +0100

    first jsx

---
+++ Week3/Day1/Practice/FristJSX/firstjsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <>
+      <h1>Hello Dojo !</h1>
+      <h2>Things I need to do</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
